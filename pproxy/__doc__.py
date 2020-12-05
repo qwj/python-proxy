7d6e43ff98c1336270bb8e62d893c4a9db6eda9d
qwj <qianwenjie@gmail.com>
2020-12-05 23:01:05 +0800

    fix version

--- pproxy/__doc__.py
+++ pproxy/__doc__.py
@@ -11,8 +11,8 @@ try:
     __version__ = get_version()
 except Exception:
     try:
-        from importlib.metadata import version
-        __version__ = version("package-name")
+        from pkg_resources import get_distribution
+        __version__ = get_distribution('pproxy').version
     except Exception:
         __version__ = 'unknown'
 
